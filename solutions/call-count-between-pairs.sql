c0bcf5803ad52a6f365fa9408e7a82b6792e66f2
SMEET PATEL <60186652+smeetpatel2530@users.noreply.github.com>
2026-06-08 15:53:29 +0530

    Add SQL query to count calls between pairs

---
+++ solutions/call-count-between-pairs.sql
@@ -0,0 +1,8 @@
+SELECT 
+    LEAST(from_id, to_id) AS person1,
+    GREATEST(from_id, to_id) AS person2,
+    COUNT(*) AS call_count,
+    SUM(duration) AS total_duration
+FROM Calls
+GROUP BY person1, person2
+ORDER BY person1, person2;
